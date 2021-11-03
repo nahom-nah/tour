b17e51cd6e3b7d2bef06a8db1bc5ee963a6fabee
nahom balcha <nahom.projectmanagement@gmail.com>
2021-11-03 10:06:56 -0700

    final 3

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -36,8 +36,8 @@ const sendErrorDev = (err, req, res) => {
     });
   }
 
-  // B) RENDERED WEBSITE
-  console.error('ERROR 💥', err);
+  
+  console.error('ERROR ', err);
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
     msg: err.message
@@ -45,37 +45,33 @@ const sendErrorDev = (err, req, res) => {
 };
 
 const sendErrorProd = (err, req, res) => {
-  // A) API
+  
   if (req.originalUrl.startsWith('/api')) {
-    // A) Operational, trusted error: send message to client
+     
     if (err.isOperational) {
       return res.status(err.statusCode).json({
         status: err.status,
         message: err.message
       });
     }
-    // B) Programming or other unknown error: don't leak error details
-    // 1) Log error
-    console.error('ERROR 💥', err);
-    // 2) Send generic message
+     
+    console.error('ERROR ', err);
+    
     return res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!'
     });
   }
-
-  // B) RENDERED WEBSITE
-  // A) Operational, trusted error: send message to client
+ 
   if (err.isOperational) {
     return res.status(err.statusCode).render('error', {
       title: 'Something went wrong!',
       msg: err.message
     });
   }
-  // B) Programming or other unknown error: don't leak error details
-  // 1) Log error
-  console.error('ERROR 💥', err);
-  // 2) Send generic message
+   
+  console.error('ERROR ', err);
+  
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
     msg: 'Please try again later.'
